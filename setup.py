80674e5c10ab4655bccf09516d5d187123ce8cd8
Milinda Pathirage <milinda.pathirage@gmail.com>
2016-07-28 23:22:41 -0400

    rename the files

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='dcv2',
-    packages=['dcv2'],
+    name='sdg',
+    packages=['sdg'],
     include_package_data=True,
     install_requires=[
         'flask',
